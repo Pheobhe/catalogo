53e815890cdb4e256f56d05fead8472d3ddec5d6
Lorena Defalco <l.defalco@mjus.gba.gob.ar>
2023-08-31 13:18:16 -0300

    configuracion de sass en vue para hacer el diseño global, especial att a vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+ 
+  css:{
+    loaderOptions: {
+      scss:{
+        additionalData:  `@import "~@/scss/styles.scss";` 
+       }
+  }
+}
+
+
+
+
+
+
+});
